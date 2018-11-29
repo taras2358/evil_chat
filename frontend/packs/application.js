ef0d2e89fc010ca94702d7df2a884de870beb782
taras2358 <taras2358@gmail.com>
2018-11-29 10:16:55 +0200

    try out react gem

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,4 +1,14 @@
+// Native components
 import "init";
 import "components/page/page";
 import "components/auth-form/auth-form";
 import "components/chat/chat";
+
+// React components
+import ReactOnRails from "react-on-rails";
+
+import HelloWorld from "components/hello_world/hello_world.jsx";
+
+ReactOnRails.register({
+  HelloWorld
+});
